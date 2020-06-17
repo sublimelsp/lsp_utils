5301097157d62100796a54611bf97630b077e2af
Rafał Chłodnicki <rchl2k@gmail.com>
2020-06-17 09:29:23 +0200

    Create separate handler for the new LSP API (#14)

--- st3/lsp_utils/__init__.py
+++ st3/lsp_utils/__init__.py
@@ -1,2 +1,16 @@
-from .npm_client_handler import NpmClientHandler
+try:
+    from LSP.plugin import __version__ as lsp_version
+except ImportError:
+    lsp_version = (0, 0, 0)
+
 from .server_npm_resource import ServerNpmResource
+
+if lsp_version >= (1, 0, 0):
+    from .npm_client_handler_v2 import NpmClientHandler
+else:
+    from .npm_client_handler import NpmClientHandler
+
+__all__ = [
+    'NpmClientHandler',
+    'ServerNpmResource',
+]
