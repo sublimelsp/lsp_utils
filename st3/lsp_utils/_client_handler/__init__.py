15dc55930976dccdb4aa5a2b11e54954be27f6da
Rafał Chłodnicki <rchl2k@gmail.com>
2020-11-17 10:30:43 +0100

    This now works with latest pyright version

--- st3/lsp_utils/_client_handler/__init__.py
+++ st3/lsp_utils/_client_handler/__init__.py
@@ -1,14 +1,10 @@
 from LSP.plugin import __version__ as lsp_version
 
 
-major_version = lsp_version[0]
-
-if major_version == 1:  # types are problematic when comparing tuples with ">=".
+if lsp_version >= (1, 0, 0):
     from .abstract_plugin import ClientHandler
-elif major_version == 0:
-    from .language_handler import ClientHandler
 else:
-    raise Exception('Unsupported LSP version {}'.format(lsp_version))
+    from .language_handler import ClientHandler
 
 __all__ = [
     'ClientHandler',
