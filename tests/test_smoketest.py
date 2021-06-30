573c275817765c5921f74d13da9c6d6044d92223
Rafal Chlodnicki <rchl2k@gmail.com>
2021-06-30 12:28:44 +0200

    blah

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -1,14 +1,22 @@
-from LSP.plugin.session_view import SessionView
 from LSP.plugin.core.typing import cast, Generator
 from .setup import TextDocumentTestCase
+
+try:
+    from LSP.plugin.session_view import SessionView
+    ST3 = False
+except ImportError:
+    ST3 = True
 
 
 class PyrightSmokeTests(TextDocumentTestCase):
 
     def test_set_and_get(self) -> Generator:
-        session_view = cast(SessionView, self.session.session_view_for_view_async(self.view))
-        self.assertIsNotNone(session_view)
-        error_region_key = session_view.diagnostics_key(1, False)
+        if ST3:
+            error_region_key = 'lsp_error'
+        else:
+            session_view = cast(SessionView, self.session.session_view_for_view_async(self.view))
+            self.assertIsNotNone(session_view)
+            error_region_key = session_view.diagnostics_key(1, False)
         error_regions = yield lambda: self.view.get_regions(error_region_key)
         self.assertEqual(len(error_regions), 1)
         region = error_regions[0]
