ddbb135100bcdc0769671105013e41ac7aa844c9
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-10 21:43:45 +0100

    Improve UvVenvManager
    - helpers: export rmtree_ex publicly
    - UvVenvManager: copy uv.lock from package if present
    - UvVenvManager: delete `.env` directory before installing to avoid errors
    - UvVenvManager: make `venv_path` public (in addition to `venv_python_path`)

--- lsp_utils/uv_venv_manager.py
+++ lsp_utils/uv_venv_manager.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from .helpers import platform_program_file_extension
+from .helpers import platform_program_file_extension, rmtree_ex
 from .uv_runner import UvRunner
 from hashlib import md5
 from pathlib import Path
@@ -9,6 +9,24 @@ import sublime
 
 
 __all__ = ['UvVenvManager']
+
+
+PYPROJECT_TOML = 'pyproject.toml'
+UV_LOCK = 'uv.lock'
+
+
+def is_hash_equal(resource_path: ResourcePath, filesystem_path: Path) -> bool:
+    if not resource_path.exists():
+        # If source resource doesn't exist then return "equal" since we don't care about that file then.
+        return True
+    if not filesystem_path.exists():
+        return False
+    source_hash = md5(resource_path.read_bytes()).hexdigest()
+    try:
+        return source_hash == md5(filesystem_path.read_bytes()).hexdigest()
+    except FileNotFoundError:
+        pass
+    return False
 
 
 @final
@@ -26,39 +44,39 @@ class UvVenvManager:
         :param storage_path:               The path of the Package Storage directory.
         """
 
-        self._package_name = package_name
-        self._project_toml_resource_path = project_toml_resource_path
+        pyproject_toml_resource_path = ResourcePath(f'Packages/{package_name}/{project_toml_resource_path}')
+        if not pyproject_toml_resource_path.exists():
+            raise Exception(f'Expected "{project_toml_resource_path}" resource not found!')
+        self._source_resource_path = pyproject_toml_resource_path.parent
         self._storage_path = storage_path
-        self._package_storage = Path(self._storage_path, self._package_name)
-        self._target_project_toml_path = self._package_storage / 'pyproject.toml'
-        self._target_venv_path = self._package_storage / '.venv'
+        self._package_storage = Path(self._storage_path, package_name)
         self._uv: UvRunner | None = None
+
+    @property
+    def venv_path(self) -> Path:
+        return self._package_storage / '.venv'
 
     @property
     def venv_python_path(self) -> Path:
         bin_dir = 'Scripts' if sublime.platform() == 'windows' else 'bin'
-        return self._target_venv_path / bin_dir / f'python{platform_program_file_extension()}'
-
-    @property
-    def _source_project_toml_resource(self) -> ResourcePath:
-        resource = ResourcePath(f'Packages/{self._package_name}/{self._project_toml_resource_path}')
-        if not resource.exists():
-            raise Exception(f'Expected "{self._project_toml_resource_path}" resource not found!')
-        return resource
+        return self.venv_path / bin_dir / f'python{platform_program_file_extension()}'
 
     def needs_install_or_update(self) -> bool:
-        source_hash = md5(self._source_project_toml_resource.read_bytes()).hexdigest()
-        try:
-            with self._target_project_toml_path.open('rb') as file:
-                target_hash = md5(file.read()).hexdigest()
-        except FileNotFoundError:
-            return True
-        return source_hash != target_hash or not self._target_venv_path.exists()
+        print(1, self.venv_path.exists())
+        print(2, is_hash_equal(self._source_resource_path / PYPROJECT_TOML, self._package_storage / PYPROJECT_TOML))
+        print(3, is_hash_equal(self._source_resource_path / UV_LOCK, self._package_storage / UV_LOCK))
+        return not self.venv_path.exists() or \
+            not is_hash_equal(self._source_resource_path / PYPROJECT_TOML, self._package_storage / PYPROJECT_TOML) or \
+            not is_hash_equal(self._source_resource_path / UV_LOCK, self._package_storage / UV_LOCK)
 
     def install(self) -> None:
         if not self._uv:
             self._uv = UvRunner(self._storage_path)
-        if self._target_project_toml_path.exists():
-            self._target_project_toml_path.unlink()
-        self._source_project_toml_resource.copy(str(self._target_project_toml_path))
+        (self._package_storage / PYPROJECT_TOML).unlink(missing_ok=True)
+        (self._package_storage / UV_LOCK).unlink(missing_ok=True)
+        rmtree_ex(self.venv_path, ignore_errors=True)
+        self._package_storage.mkdir(parents=True, exist_ok=True)
+        (self._source_resource_path / PYPROJECT_TOML).copy(str(self._package_storage / PYPROJECT_TOML))
+        if (self._source_resource_path / UV_LOCK).exists():
+            (self._source_resource_path / UV_LOCK).copy(self._package_storage / UV_LOCK)
         self._uv.run_command('sync', cwd=str(self._package_storage))
