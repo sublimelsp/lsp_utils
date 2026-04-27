2e6e5175bf7fead77859ddff30e6c927413a9f2b
Rafał Chłodnicki <rchl2k@gmail.com>
2026-04-27 19:26:20 +0200

    breaking: remove api decorators (#163)

--- lsp_utils/_client_handler/__init__.py
+++ lsp_utils/_client_handler/__init__.py
@@ -1,11 +1,7 @@
 from __future__ import annotations
 
 from .abstract_plugin import ClientHandler
-from .api_decorator import notification_handler
-from .api_decorator import request_handler
 
 __all__ = [
     'ClientHandler',
-    'notification_handler',
-    'request_handler',
 ]
