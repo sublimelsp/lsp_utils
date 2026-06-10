2c349a6418283c7f49de2aaa9dda9bd2f560079e
Rafal Chlodnicki <rchl2k@gmail.com>
2026-06-10 07:47:51 +0200

    Fix tests

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .setup import GeneratorAny
 from .setup import TextDocumentTestCase
 from .setup import TIMEOUT_TIME
+from LSP.protocol import DiagnosticSeverity
 from typing import cast
 from typing import TYPE_CHECKING
 from typing_extensions import override
@@ -14,15 +15,19 @@ if TYPE_CHECKING:
 class BaseTestCase(TextDocumentTestCase):
 
     def test_diagnostics(self) -> GeneratorAny:
-        session_view = cast('SessionView', self.session.session_view_for_view_async(self.view))
+        session = self.session
+        assert session, 'Expected Session'
+        session_view = cast('SessionView', session.session_view_for_view_async(self.view))
         assert session_view is not None
-        error_region_key = f'{session_view.diagnostics_key(1, multiline=False)}_icon'
+        error_region_key = f'{session_view.diagnostics_key(DiagnosticSeverity.Error, multiline=False)}_icon'
         yield {'condition': lambda: len(session_view.session_buffer.diagnostics) == 1, 'timeout': TIMEOUT_TIME * 4}
         error_regions = self.view.get_regions(error_region_key)
         assert len(error_regions) == 1
         region = error_regions[0]
         assert (region.a, region.b) == (6, 7)
-        self.view.window().run_command('show_panel', {"panel": "console", "toggle": True})
+        window = self.view.window()
+        assert window, 'Expected Window to be present'
+        window.run_command('show_panel', {"panel": "console", "toggle": True})
 
 
 class SystemRuntime(BaseTestCase):
@@ -43,18 +48,5 @@ class LocalNodeRuntime(BaseTestCase):
     def setUpClass(cls) -> GeneratorAny:
         cls.set_lsp_utils_settings({
             'nodejs_runtime': ['local'],
-            'local_use_electron': False,
-        })
-        yield from super().setUpClass()
-
-
-class LocalElectronRuntime(BaseTestCase):
-
-    @classmethod
-    @override
-    def setUpClass(cls) -> GeneratorAny:
-        cls.set_lsp_utils_settings({
-            'nodejs_runtime': ['local'],
-            'local_use_electron': True,
         })
         yield from super().setUpClass()
